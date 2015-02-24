33aaa9530ea5f60227597eae4832f845fc5eb97e
jordilaforge <8899ph@web.de>
2015-02-25 00:15:08 +0100

    Lab02: --Added Task 2-4 --Green Unit Tests --ToDo Sort List Task 2e

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -13,11 +13,14 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RoutePlanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
-            var Bern = new WayPoint("Bern",46.951081, 7.438637);
-            var Tripolis = new WayPoint("Tripolis",32.883333333333, 13.166666666667);
+            var bern = new WayPoint("Bern",46.951081, 7.438637);
+            var tripolis = new WayPoint("Tripolis",32.883333333333, 13.166666666667);
             var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            var City = new City("Bern", "Schweiz", 75000, 47.479319847061966, 8.212966918945312);
             Console.WriteLine(wayPoint);
-            Console.WriteLine("Distance Bern to Tripolis: "+Bern.Distance(Tripolis)+"km");
+            Console.WriteLine("Distance Bern to Tripolis: "+bern.Distance(tripolis)+"km");
+            Cities bla = new Cities();
+            Console.WriteLine(bla.ReadCities(@"..\..\..\RoutePlannerTest\data\citiesTestDataLab2.txt"));
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
         }
--- RoutePlannerLib/WayPoint.cs
+++ RoutePlannerLib/WayPoint.cs
@@ -11,29 +11,39 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
         public string Name { get; set; }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
-        public WayPoint(string _name, double _latitude, double _longitude)
+        public WayPoint(string name, double latitude, double longitude)
         {
-            Name = _name;
-            Latitude = _latitude;
-            Longitude = _longitude;
+            this.Name = name;
+            this.Latitude = latitude;
+            this.Longitude = longitude;
         }
 
         public override String ToString()
         {
-            if (this.Name != null || !(this.Name.Equals("")))
+            if (this.Name == null || this.Name.Equals(""))
             {
-                return "Waypoint: " + this.Name + " " + Math.Round(this.Latitude, 2) + "/" + Math.Round(this.Longitude, 2);
+                return "WayPoint: " + Math.Round(this.Latitude, 2) + "/" + Math.Round(this.Longitude, 2);
+                
             }
             else
             {
-                return "Waypoint: " + Math.Round(this.Latitude, 2) + "/" + Math.Round(this.Longitude, 2);
+                return "WayPoint: " + this.Name + " " + Math.Round(this.Latitude, 2) + "/" + Math.Round(this.Longitude, 2);
             }
         }
 
         public double Distance(WayPoint target)
         {
-            int radius = 6371;
-            return radius * Math.Acos(Math.Sin(this.Latitude)*Math.Sin(target.Latitude)+Math.Cos(this.Latitude)*Math.Cos(target.Latitude)*Math.Cos(this.Longitude-target.Longitude));
+            int Radius = 6371;
+            double LatitudeThis = ConvertToRadians(this.Latitude);
+            double LongitudeThis = ConvertToRadians(this.Longitude);
+            double LatitudeTarget = ConvertToRadians(target.Latitude);
+            double LongitudeTarget = ConvertToRadians(target.Longitude);
+            return Radius * Math.Acos(Math.Sin(LatitudeThis) * Math.Sin(LatitudeTarget) + Math.Cos(LatitudeThis) * Math.Cos(LatitudeTarget) * Math.Cos(LongitudeThis - LongitudeTarget));
+        }
+
+        public double ConvertToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
         }
     }
 }
