ab3371a36c5cd435151dd723e6ed35cb13044f2a
jordilaforge <8899ph@web.de>
2015-03-01 19:07:14 +0100

    Lab03: -Added missing Files

---
+++ RoutePlannerLib/RouteRequestEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RouteRequestEventArgs : EventArgs
+    {
+        public String FromCity { get; set; }
+        public String ToCity { get; set; }
+        public TransportModes Mode { get; set; }
+
+        public RouteRequestEventArgs(String fromCity, String toCity, TransportModes mode)
+        {
+            this.FromCity = fromCity;
+            this.ToCity = toCity;
+            this.Mode = mode;
+        }
+
+
+
+        
+    }
+}
---
+++ RoutePlannerLib/RouteRequestWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RouteRequestWatcher
+       {
+        private string id;
+        public RouteRequestWatcher(string ID, Routes pub)
+        {
+            id = ID;
+            // Subscribe to the event using C# 2.0 syntax
+            pub.RouteRequestEvent += LogRouteRequest;
+        }
+
+        public RouteRequestWatcher()
+        {
+            
+        }
+
+        // Define what actions to take when the event is raised.
+        public void LogRouteRequest(object sender, RouteRequestEventArgs e)
+        {
+            Console.WriteLine(id + " received this message: {0}", e.FromCity);
+        }
+
+        public object GetCityRequests(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+
+       }
+}
