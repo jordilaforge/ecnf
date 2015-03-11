67034065c27f04c4f8094c35218c9c6356575ba0
jordilaforge <8899ph@web.de>
2015-03-11 20:21:19 +0100

    Lab04: -Added Extension Method no finished yet

---
+++ RoutePlannerLib/TextReaderExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    class TextReaderExtension
+    {
+        public static IEnumerable<string> GetSplitteLines(this string text, char ch)
+        {
+            var parts = text.Split(new char[] { ch });
+            var result = new StringBuilder();
+            foreach (var part in parts)
+                yield return part;
+        }
+    }
+}
