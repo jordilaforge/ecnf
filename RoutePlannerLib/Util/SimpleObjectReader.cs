d2564cf8cdf6e5f56288d826f878a908fd160b35
jordilaforge <8899ph@web.de>
2015-03-18 17:27:22 +0100

    Lab05: -Implemented dummy Serializer and

---
+++ RoutePlannerLib/Util/SimpleObjectReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    public class SimpleObjectReader
+    {
+        private System.IO.StringReader stream;
+
+        public SimpleObjectReader(System.IO.StringReader stream)
+        {
+            this.stream = stream;
+        }
+
+        public City Next()
+        {
+            stream.ReadLine();
+            Regex regex = new Regex("\"(.*)\"");
+            var v = regex.Match(stream.ReadLine());
+            String cName = (v.Groups[1].ToString());
+            Console.WriteLine(cName);
+            var v2 = regex.Match(stream.ReadLine());
+            String cCountry = (v2.Groups[1].ToString());
+            Console.WriteLine(cCountry);
+            var v3 = stream.ReadLine();
+            int population = Convert.ToInt32(v3.Substring(v3.LastIndexOf('=') + 1));
+            Console.WriteLine(population);
+            stream.ReadLine();
+            stream.ReadLine();
+            stream.ReadLine();
+            var v4 = stream.ReadLine();
+            double longitude = Convert.ToDouble(v4.Substring(v4.LastIndexOf('=') + 1));
+            Console.WriteLine(longitude);
+            var v5 = stream.ReadLine();
+            double latitude = Convert.ToDouble(v5.Substring(v5.LastIndexOf('=') + 1));
+            Console.WriteLine(latitude);
+            stream.ReadLine();
+            stream.ReadLine();
+            City c = new City(cName, cCountry, population, latitude, longitude);
+            return c;
+        }
+    }
+}
---
+++ RoutePlannerLib/Util/SimpleObjectWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    public class SimpleObjectWriter
+    {
+        private StringWriter stream;
+
+        public SimpleObjectWriter(StringWriter stream)
+        {
+            this.stream = stream;
+        }
+
+
+
+        public void Next(City c)
+        {
+
+            stream.WriteLine("Instance of "+c.GetType().FullName);
+            stream.WriteLine("Name=" +"\"" +c.Name+"\"");
+            stream.WriteLine("Country=" + "\"" + c.Country + "\"");
+            stream.WriteLine("Population=" + c.Population);
+            stream.WriteLine("Location is a nested object...");
+            stream.WriteLine("Instance of " + c.Location.GetType().FullName);
+            stream.WriteLine("Name=" + "\"" + c.Location.Name + "\"");
+            stream.WriteLine("Longitude=" + c.Location.Longitude);
+            stream.WriteLine("Latitude=" + c.Location.Latitude);
+            stream.WriteLine("End of instance");
+            stream.WriteLine("End of instance");
+            Console.WriteLine(stream);
+        }
+    }
+}
