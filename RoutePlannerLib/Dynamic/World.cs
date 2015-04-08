99fa04b17fe541aa99d60af1a81450822dd35168
jordilaforge <8899ph@web.de>
2015-04-08 22:04:44 +0200

    Lab07: -Added Excel Export

---
+++ RoutePlannerLib/Dynamic/World.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Dynamic
+{
+    public class World
+    {
+        private Cities cities;
+
+        public World(Cities cities)
+        {
+            // TODO: Complete member initialization
+            this.cities = cities;
+        }
+    }
+}
